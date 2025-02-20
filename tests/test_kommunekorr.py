49c337c07f7114d8eaeb110d13c2c9ece4888f25
Arne Sørli <81353974+arneso-ssb@users.noreply.github.com>
2025-02-20 13:19:43 +0100

    Fix tests

--- tests/test_kommunekorr.py
+++ tests/test_kommunekorr.py
@@ -1,31 +1,31 @@
-# import pandas as pd
+import pandas as pd
 
-# from ssb_kostra_python import kommunekorr
+from ssb_kostra_python.kommunekorr import kostra_kommunekorr
 
 
-# def test_kostra_kommunekorr(mock_klass_classification, mock_klass_correspondence):
-#     year = "2023"
-#     result = kostra_kommunekorr(year)
-#     assert isinstance(result, pd.DataFrame)
-#     assert result.shape[0] == 7
+def test_kostra_kommunekorr(mock_klass_classification, mock_klass_correspondence):
+    year = "2023"
+    result = kostra_kommunekorr(year)
+    assert isinstance(result, pd.DataFrame)
+    assert result.shape[0] == 7
 
-#     exp_cols = [
-#         "kom_nr",
-#         "kom_navn",
-#         "fylke_nr",
-#         "fylke_navn",
-#         "fylke_nr_eka",
-#         "fylke_nr_eka_m_tekst",
-#         "fylke_validFrom",
-#         "fylke_validTo",
-#         "kostra_gr",
-#         "kostra_gr_navn",
-#         "kostra_validFrom",
-#         "kostra_validTo",
-#         "landet",
-#         "landet_u_oslo",
-#     ]
-#     assert result.shape[1] == len(exp_cols)
+    exp_cols = [
+        "kom_nr",
+        "kom_navn",
+        "fylke_nr",
+        "fylke_navn",
+        "fylke_nr_eka",
+        "fylke_nr_eka_m_tekst",
+        "fylke_validFrom",
+        "fylke_validTo",
+        "kostra_gr",
+        "kostra_gr_navn",
+        "kostra_validFrom",
+        "kostra_validTo",
+        "landet",
+        "landet_u_oslo",
+    ]
+    assert result.shape[1] == len(exp_cols)
 
-#     for c1, c2 in zip(exp_cols, result.columns, strict=False):
-#         assert c1 == c2
+    for c1, c2 in zip(exp_cols, result.columns, strict=False):
+        assert c1 == c2
