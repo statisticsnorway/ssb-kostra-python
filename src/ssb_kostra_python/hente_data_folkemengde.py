f6b3aed2d269f6efad55874f2bd3d496ab194130
ssb-coh <coh@ssb.no>
2026-06-26 15:01:21 +0200

    fix mypy kommune dataframe handling

--- src/ssb_kostra_python/hente_data_folkemengde.py
+++ src/ssb_kostra_python/hente_data_folkemengde.py
@@ -135,14 +135,17 @@ def hente_data_folkemengde(
     #         hjelpefunksjoner._hent_folkemengde_kommune_31_12(int(kildeaar))
     #     )
     elif regionsnivaa.lower() == "kommune":
-        _, folkemengde_31_12 = hjelpefunksjoner._hent_folkemengde_kommune_31_12(
+        _, folkemengde_kommune = hjelpefunksjoner._hent_folkemengde_kommune_31_12(
             int(kildeaar)
         )
 
-        if folkemengde_31_12 is None:
+        if folkemengde_kommune is None:
             raise RuntimeError(
                 f"Klarte ikke å lage KOSTRA-aggregert folkemengdefil for {kildeaar}."
             )
+
+        folkemengde_31_12 = folkemengde_kommune
+
     elif regionsnivaa.lower() == "fylkeskommune":
         folkemengde_31_12 = (
             #     hente_lage_folkemengdefil_31_12._hent_folkemengde_fylkeskommune_31_12(
