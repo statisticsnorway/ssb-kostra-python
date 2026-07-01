64f86be019198f84afa55d87184f6196416a9d55
ssb-coh <coh@ssb.no>
2026-07-01 15:29:22 +0200

    lagt på patch, forhåndsdefinering i eksempelarkene

--- examples/aggregere_alder_eksempel.py
+++ examples/aggregere_alder_eksempel.py
@@ -5,7 +5,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.19.1
+#       jupytext_version: 1.19.3
 #   kernelspec:
 #     display_name: ssb-kostra-python
 #     language: python
@@ -25,6 +25,8 @@ import pandas as pd
 from fagfunksjoner import latest_version_path
 
 INPUT_PATCH_TARGET = "builtins.input"
+from unittest.mock import patch
+
 from IPython.display import display  # for nice tables in notebooks
 
 from ssb_kostra_python.summere_til_aldersgrupperinger import (
@@ -33,20 +35,40 @@ from ssb_kostra_python.summere_til_aldersgrupperinger import (
 
 # %% [markdown]
 # ### Henter først inn et datasett vi kan jobbe med, som inneholder befolkning fordelt på region, kjønn og alder.
+# ### Du kan selv bestemme hvilket statistikkår tabellen skal gjelde ved å endre på "statistikkaar = 20XX"
 
 # %%
+# Bestemmer først statistikkår
+statistikkaar = 2024
 # Definerer en filsti. "latest_version_path" (pakke lastet ned over) sørger for å identifisere siste versjon av datasettet.
-filsti_folkemengde_bydeler_2024 = latest_version_path(
-    "/buckets/delt-kostra-befolkning-delt/bydeler/2024/folkmengde_bydeler_p2024-12-31"
+filsti_folkemengde_bydeler = latest_version_path(
+    f"/buckets/delt-kostra-befolkning-delt/bydeler/{statistikkaar}/folkmengde_bydeler_p{statistikkaar}-12-31"
 )
 # Leser selve filen. Denne er lagret som en parquet-fil.
-folketall_bydeler = pd.read_parquet(filsti_folkemengde_bydeler_2024)
+folketall_bydeler = pd.read_parquet(filsti_folkemengde_bydeler)
 # Viser datasettet.
 display(folketall_bydeler)
 
 # %% [markdown]
 # ### Summerer opp datasettet "folketall_bydeler" til KOSTRA-aldersgrupperinger
+# ### Funksjonen trenger å vite klassifikasjonsvariablene for å aggregere riktig.
 
 # %%
 # Summerer til KOSTRA-aldersgrupperinger
 folketall_bydeler_alder = summere_til_aldersgrupperinger(folketall_bydeler)
+
+# %% [markdown]
+# #### Det kan være slitsomt å måtte taste inn klassifikasjonsvariablene hver gang funksjonen kjøres.
+# #### Når du setter opp et produksjonsløp og du vet hvilke klassifikasjonsvariable som inngår når funksjonen kjøres, kan du sette opp koden som vist under for å unngå dette.
+# #### Du forhåndsdefinerer klassifikasjonsvariablene i "predefined_input". Deretter kopler du den opprinnelige funksjonen til de forhåndsdefinerte inputene som vist under. Funksjonen vil ta i bruk de forhåndsdefinerte inputene og kjøre uten å be deg taste dem inn.
+
+# %%
+predefined_input = "kjonn, alder, to"
+
+with patch(INPUT_PATCH_TARGET, return_value=predefined_input):
+    folketall_bydeler_alder = summere_til_aldersgrupperinger(folketall_bydeler)
+
+# Viser datasettet
+display(folketall_bydeler_alder)
+
+# %%
--- examples/aggregere_kjonn_eksempel.py
+++ examples/aggregere_kjonn_eksempel.py
@@ -5,7 +5,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.19.1
+#       jupytext_version: 1.19.3
 #   kernelspec:
 #     display_name: ssb-kostra-python
 #     language: python
@@ -18,11 +18,12 @@
 # ### Vi laster den inn med "from ssb_kostra_python import summere_kjonn".
 
 # %%
-
 import pandas as pd
 from fagfunksjoner import latest_version_path
 
 INPUT_PATCH_TARGET = "builtins.input"
+from unittest.mock import patch
+
 from IPython.display import display  # for nice tables in notebooks
 
 from ssb_kostra_python import summere_kjonn
@@ -31,12 +32,14 @@ from ssb_kostra_python import summere_kjonn
 # ### Henter først inn et datasett vi kan jobbe med, som inneholder befolkning fordelt på region, kjønn og alder.
 
 # %%
+# Bestemmer først statistikkår
+statistikkaar = 2024
 # Definerer en filsti. "latest_version_path" (pakke lastet ned over) sørger for å identifisere siste versjon av datasettet.
-filsti_folkemengde_bydeler_2024 = latest_version_path(
-    "/buckets/delt-kostra-befolkning-delt/bydeler/2024/folkmengde_bydeler_p2024-12-31"
+filsti_folkemengde_bydeler = latest_version_path(
+    f"/buckets/delt-kostra-befolkning-delt/bydeler/{statistikkaar}/folkmengde_bydeler_p{statistikkaar}-12-31"
 )
 # Leser selve filen. Denne er lagret som en parquet-fil.
-folketall_bydeler = pd.read_parquet(filsti_folkemengde_bydeler_2024)
+folketall_bydeler = pd.read_parquet(filsti_folkemengde_bydeler)
 # Viser datasettet.
 display(folketall_bydeler)
 
@@ -44,4 +47,18 @@ display(folketall_bydeler)
 # Kjører funksjonen. folketall_bydeler_sum_kjonn er det endelige datasettet som genereres.
 folketall_bydeler_sum_kjonn = summere_kjonn.summere_over_kjonn(folketall_bydeler)
 # Viser det genererte datasettet. Du vil se at kolonnen for kjønn er borte, for nå er kjønnene summert opp.
+display(folketall_bydeler_sum_kjonn)
+
+# %% [markdown]
+# ### Det kan være slitsomt å måtte taste inn klassifikasjonsvariablene hver gang funksjonen kjøres.
+# ### Når du setter opp et produksjonsløp og du vet hvilke klassifikasjonsvariable som inngår når funksjonen kjøres, kan du sette opp koden som vist under for å unngå dette.
+# ### Du forhåndsdefinerer klassifikasjonsvariablene i “predefined_input”. Deretter kopler du den opprinnelige funksjonen til de forhåndsdefinerte inputene som vist under. Funksjonen vil ta i bruk de forhåndsdefinerte inputene og kjøre uten å be deg taste dem inn.
+
+# %%
+predefined_input = "kjonn, alder"
+
+with patch(INPUT_PATCH_TARGET, return_value=predefined_input):
+    folketall_bydeler_sum_kjonn = summere_kjonn.summere_over_kjonn(folketall_bydeler)
+
+# Viser datasettet
 display(folketall_bydeler_sum_kjonn)
